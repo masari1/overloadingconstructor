c5f0750b55431960fc81f93443439e6d40794fe8
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 17:21:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main (String [] args){
+
+
+        Pizza pizza = new Pizza("thicc crust");
+
+        System.out.println("Here are the ingredients: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.sauße);
+        System.out.println(pizza.topping);
+
+
+
+    }
+}
---
+++ src/Pizza.java
@@ -0,0 +1,39 @@
+public class Pizza {
+
+
+    String bread;
+    String sauße;
+    String cheese;
+    String topping;
+
+
+
+    Pizza(String bread) {
+        this.bread = bread;
+
+
+    }
+
+    Pizza(String bread, String sauße) {
+        this.bread = bread;
+        this.sauße = sauße;
+    }
+
+        Pizza(String bread, String sauße, String cheese) {
+            this.bread = bread;
+            this.sauße = sauße;
+            this.cheese = cheese;
+
+
+        }
+
+        Pizza(String bread, String sauße, String cheese, String topping) {
+            this.bread = bread;
+            this.sauße = sauße;
+            this.cheese = cheese;
+            this.topping = topping;
+
+        }
+
+
+    }
